2c50b9d7ffb9535bd2104faeb919de3b1985e09f
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-07-29 09:39:02 -0400

    added css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var firebase
-
+var moment
 // Initialize Firebase
 var config = {
   apiKey: 'AIzaSyCO-ssItvvVxEW7FAl8nBwwbkOFeAxcVUc',
@@ -11,12 +11,63 @@ var config = {
 }
 firebase.initializeApp(config)
 
-// Create a variable to reference the database
 var database = firebase.database()
-// Initial Values
-var moment
 
 // Clock  
 setInterval(function () {
   $('.current-time').text(moment().format('dddd, MMMM Do, kk:mm:ss'))
 }, 1000)
+
+// Button for adding train
+$('#add-train-btn').on('click', function (event) {
+  event.preventDefault()
+  // Grabs user input
+  var trainName = $('#train-name-input').val().trim()
+  var trainDestination = $('#destination-input').val().trim()
+  var trainFirst = $('#first-train-input').val().trim()
+  var trainFrequency = $('#frequency-input').val().trim()
+
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    first: trainFirst,
+    frequency: trainFrequency
+  }
+  // Uploads train data to the database
+  database.ref().push(newTrain)
+
+  // Logs everything to console
+  console.log(newTrain.name)
+  console.log(newTrain.destination)
+  console.log(newTrain.first)
+  console.log(newTrain.frequency)
+
+  // Alert
+  alert('Train successfully added')
+
+  // Clears all of the text-boxes
+  $('#train-name-input').val('')
+  $('#destination-input').val('')
+  $('#first-train-input').val('')
+  $('#frequency-input').val('')
+})
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on('child_added', function (childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val())
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name
+  var trainDestination = childSnapshot.val().destination
+  var trainFirst = childSnapshot.val().first
+  var trainFrequency = childSnapshot.val().frequency
+
+  // Train Info
+  console.log(trainName)
+  console.log(trainDestination)
+  console.log(trainFirst)
+  console.log(trainFrequency)
+
+  // Add each train's data into the table
+  $('#schedule-table > tbody').append('<tr><td>' + trainName + '</td><td>' + trainDestination + '</td><td>' + trainFrequency + '</td></tr>')
+})
